25a31fe40175525df38e9ebe823afe61fb2f0084
JohnSwiftC <johnswift@live.com>
2025-04-28 17:48:22 -0500

    add hint to next 'hack'

--- src/ProgramWindowComps/AboutMe.jsx
+++ src/ProgramWindowComps/AboutMe.jsx
@@ -22,6 +22,11 @@ export default function AboutMe() {
                         Outside being a loser I have 2 dogs, Jake and Marco. Along with my dogs I have a girlfriend whose cat Gus is constantly fighting for my attention. And winning.
                         Wrestled all 4 years in highschool, made varsity in the latter 3, finishing up as team captain.
                     </p>
+                    <br></br>
+                    <p>
+                        Also, if you're still playing along, go look at the cat command. Rumor has it the developer disabled every single compiler
+                        security feature and also made the filename buffer only 16 bytes. Rumor also has it that a magic login function is at address ACAT ;)
+                    </p>
                 </div>
                 <div className="basis-1/2 flex flex-cols justify-center m-2">
                     <div className="text-gray-300 font">
